4b59bfb823c72fede2dd2e7fa1412d9f3f867ac3
Ryan McGeary <ryan@mcgeary.org>
2026-04-01 08:43:16 -0600

    Fix #stats to prevent double formatting and use proper percent literal

--- lib/bloomfilter/filter.rb
+++ lib/bloomfilter/filter.rb
@@ -2,12 +2,12 @@ module BloomFilter
   class Filter
     def stats
       fp = ((1.0 - Math.exp(-(@opts[:hashes] * size).to_f / @opts[:size])) ** @opts[:hashes]) * 100
-      printf "Number of filter buckets (m): %d\n" % @opts[:size]
-      printf "Number of bits per buckets (b): %d\n" % @opts[:bucket]
-      printf "Number of filter elements (n): %d\n" % size
-      printf "Number of filter hashes (k) : %d\n" % @opts[:hashes]
-      printf "Raise on overflow? (r) : %s\n" % @opts[:raise].to_s
-      printf "Predicted false positive rate = %.2f%\n" % fp
+      printf "Number of filter buckets (m): %d\n", @opts[:size]
+      printf "Number of bits per buckets (b): %d\n", @opts[:bucket]
+      printf "Number of filter elements (n): %d\n", size
+      printf "Number of filter hashes (k) : %d\n", @opts[:hashes]
+      printf "Raise on overflow? (r) : %s\n", @opts[:raise].to_s
+      printf "Predicted false positive rate = %.2f%%\n", fp
     end
   end
 end
