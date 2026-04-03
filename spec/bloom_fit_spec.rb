97a73cc9e970cb3168b1857a78e0e7000a93cca4
Ryan McGeary <ryan@mcgeary.org>
2026-04-03 09:58:01 -0600

    Fix RSpec/ExampleWording cop

--- spec/bloom_fit_spec.rb
+++ spec/bloom_fit_spec.rb
@@ -1,7 +1,7 @@
 require "helper"
 
 describe BloomFit do
-  it "should clear" do
+  it "clears" do
     bf = BloomFit.new(size: 100, hashes: 2)
     bf.insert("test")
     expect(bf.include?("test")).to be true
@@ -9,7 +9,7 @@ describe BloomFit do
     expect(bf.include?("test")).to be false
   end
 
-  it "should merge" do
+  it "merges" do
     bf1 = BloomFit.new(size: 100, hashes: 2)
     bf2 = BloomFit.new(size: 100, hashes: 2)
     bf2.insert("test")
@@ -19,102 +19,100 @@ describe BloomFit do
     expect(bf2.include?("test")).to be true
   end
 
-  context "behave like a bloom filter" do
-    it "should test set membership" do
-      bf = BloomFit.new(size: 100, hashes: 2)
-      bf.insert("test")
-      bf.insert("test1")
+  it "tests set membership" do
+    bf = BloomFit.new(size: 100, hashes: 2)
+    bf.insert("test")
+    bf.insert("test1")
 
-      expect(bf.include?("test")).to be true
-      expect(bf.include?("abcd")).to be false
-      expect(bf.include?("test", "test1")).to be true
-    end
-
-    it "should work with any object's to_s" do
-      subject.insert(:test)
-      subject.insert(:test1)
-      subject.insert(12_345)
-
-      expect(subject.include?("test")).to be true
-      expect(subject.include?("abcd")).to be false
-      expect(subject.include?("test", "test1", "12345")).to be true
-    end
-
-    it "should return the number of bits set to 1" do
-      bf = BloomFit.new(hashes: 4)
-      bf.insert("test")
-      expect(bf.set_bits).to be == 4
-
-      bf = BloomFit.new(hashes: 1)
-      bf.insert("test")
-      expect(bf.set_bits).to be == 1
-    end
-
-    it "should return intersection with other filter" do
-      bf1 = BloomFit.new
-      bf1.insert("test")
-      bf1.insert("test1")
-
-      bf2 = BloomFit.new
-      bf2.insert("test")
-      bf2.insert("test2")
-
-      bf3 = bf1 & bf2
-      expect(bf3.include?("test")).to be true
-      expect(bf3.include?("test1")).to be false
-      expect(bf3.include?("test2")).to be false
-    end
-
-    it "should raise an exception when intersection is to be computed for incompatible filters" do
-      bf1 = BloomFit.new(size: 10)
-      bf1.insert("test")
-
-      bf2 = BloomFit.new(size: 20)
-      bf2.insert("test")
-
-      expect { bf1 & bf2 }.to raise_error(BloomFit::ConfigurationMismatch)
-    end
-
-    it "should return union with other filter" do
-      bf1 = BloomFit.new
-      bf1.insert("test")
-      bf1.insert("test1")
-
-      bf2 = BloomFit.new
-      bf2.insert("test")
-      bf2.insert("test2")
-
-      bf3 = bf1 | bf2
-      expect(bf3.include?("test")).to be true
-      expect(bf3.include?("test1")).to be true
-      expect(bf3.include?("test2")).to be true
-    end
-
-    it "should raise an exception when union is to be computed for incompatible filters" do
-      bf1 = BloomFit.new(size: 10)
-      bf1.insert("test")
-
-      bf2 = BloomFit.new(size: 20)
-      bf2.insert("test")
-
-      expect {bf1 | bf2}.to raise_error(BloomFit::ConfigurationMismatch)
-    end
-
-    it "should output current stats" do
-      subject.insert("test")
-      expect { subject.stats }.not_to raise_error
-    end
+    expect(bf.include?("test")).to be true
+    expect(bf.include?("abcd")).to be false
+    expect(bf.include?("test", "test1")).to be true
   end
 
-  context "serialize" do
+  it "works with any object's to_s" do
+    subject.insert(:test)
+    subject.insert(:test1)
+    subject.insert(12_345)
+
+    expect(subject.include?("test")).to be true
+    expect(subject.include?("abcd")).to be false
+    expect(subject.include?("test", "test1", "12345")).to be true
+  end
+
+  it "returns the number of bits set to 1" do
+    bf = BloomFit.new(hashes: 4)
+    bf.insert("test")
+    expect(bf.set_bits).to be == 4
+
+    bf = BloomFit.new(hashes: 1)
+    bf.insert("test")
+    expect(bf.set_bits).to be == 1
+  end
+
+  it "returns intersection with other filter" do
+    bf1 = BloomFit.new
+    bf1.insert("test")
+    bf1.insert("test1")
+
+    bf2 = BloomFit.new
+    bf2.insert("test")
+    bf2.insert("test2")
+
+    bf3 = bf1 & bf2
+    expect(bf3.include?("test")).to be true
+    expect(bf3.include?("test1")).to be false
+    expect(bf3.include?("test2")).to be false
+  end
+
+  it "raises an exception when intersection is to be computed for incompatible filters" do
+    bf1 = BloomFit.new(size: 10)
+    bf1.insert("test")
+
+    bf2 = BloomFit.new(size: 20)
+    bf2.insert("test")
+
+    expect { bf1 & bf2 }.to raise_error(BloomFit::ConfigurationMismatch)
+  end
+
+  it "returns union with other filter" do
+    bf1 = BloomFit.new
+    bf1.insert("test")
+    bf1.insert("test1")
+
+    bf2 = BloomFit.new
+    bf2.insert("test")
+    bf2.insert("test2")
+
+    bf3 = bf1 | bf2
+    expect(bf3.include?("test")).to be true
+    expect(bf3.include?("test1")).to be true
+    expect(bf3.include?("test2")).to be true
+  end
+
+  it "raises an exception when union is to be computed for incompatible filters" do
+    bf1 = BloomFit.new(size: 10)
+    bf1.insert("test")
+
+    bf2 = BloomFit.new(size: 20)
+    bf2.insert("test")
+
+    expect {bf1 | bf2}.to raise_error(BloomFit::ConfigurationMismatch)
+  end
+
+  it "outputs current stats" do
+    subject.insert("test")
+    expect { subject.stats }.not_to raise_error
+  end
+
+  context "serialization" do
     after(:each) { File.unlink("bf.out") }
 
-    it "should marshall" do
+    it "marshalls" do
       bf = BloomFit.new
       expect { bf.save("bf.out") }.not_to raise_error
     end
 
-    it "should load from marshalled" do
+    it "loads from marshalled" do
       subject.insert("foo")
       subject.insert("bar")
       subject.save("bf.out")
