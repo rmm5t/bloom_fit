2b12c0f56f3e854b2a4e98c81dd29e39de813278
Ryan McGeary <ryan@mcgeary.org>
2026-04-03 08:41:29 -0600

    Fix example

--- examples/simple-native.rb
+++ examples/simple-native.rb
@@ -4,22 +4,22 @@ require 'bloom_fit'
 WORDS = %w(duck penguin bear panda)
 TEST = %w(penguin moose racooon)
 
-bf = BloomFit.new(:size => 100, :hashes => 2, :bucket => 3, :raise => false)
+bf = BloomFit.new(size: 1000, hashes: 2)
 
 WORDS.each { |w| bf.insert(w) }
 TEST.each do |w|
   puts "#{w}: #{bf.include?(w)}"
 end
 
+puts
 bf.stats
 
-#  penguin: true
-#  moose: false
-#  racooon: false
-#
-#  Number of filter buckets (m): 100
-#  Number of bits per buckets (b): 1
-#  Number of filter elements (n): 4
-#  Number of filter hashes (k) : 4
-#  Raise on overflow? (r) : false
-#  Predicted false positive rate = 0.05%
+# penguin: true
+# moose: false
+# racooon: false
+
+# Number of filter buckets (m): 1000
+# Number of bits per buckets (b): 1
+# Number of set bits (n): 8
+# Number of filter hashes (k) : 2
+# Predicted false positive rate = 0.03%
